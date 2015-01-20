6e36c936978c6f728c5532b616307b0bc4b86dbb
Samuel Reed <samuel.trace.reed@gmail.com>
2015-01-20 14:29:29 +0100

    Remove perf export in examples

--- test/test-hook.jsx
+++ test/test-hook.jsx
@@ -4,7 +4,6 @@ require('style!css!../css/styles.css');
 require('style!css!../examples/example-styles.css');
 require('style!css!../node_modules/react-resizable/css/styles.css');
 typeof window !== "undefined" && (window.React = React); // for devtools
-typeof window !== "undefined" && (window.Perf = React.addons.Perf); // for devtools
 
 module.exports = function(Layout) {
   document.addEventListener("DOMContentLoaded", function(event) { 
