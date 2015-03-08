0e041979354b40f2b1d0d7826b4b0e51a33aec9f
Samuel Reed <samuel.trace.reed@gmail.com>
2015-03-08 15:04:38 +0700

    Remove build directory and check-build, instead use npm prepublish.

--- check-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ALL=$(cat build/*.js)
-make js
-COMPARE=$(cat build/*.js)
-if [ "$ALL" != "$COMPARE" ]; then
-	echo -e "\nERROR: The build must be run before committing! It has been run for you. Please add the files to the commit.\n";
-	exit 1;
-fi;
