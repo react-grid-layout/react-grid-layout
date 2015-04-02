51343fda7f5db103336bec98fab8d4febbe0f7d5
Samuel Reed <samuel.trace.reed@gmail.com>
2015-04-02 15:00:32 -0400

    Update to babel 5

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf ./build
-./node_modules/.bin/babel --experimental ./lib --out-dir ./build
+./node_modules/.bin/babel --stage 0 --out-dir ./build ./lib
 # More cross-platform compatible than `rename`
 find ./build -type f -name '*.jsx' -exec sh -c 'mv -f $0 ${0%.jsx}.js' {} \;
 
