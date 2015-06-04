5215b62dfab795af8c9ff95267daa39cbd1aadb8
Samuel Reed <samuel.trace.reed@gmail.com>
2015-06-03 19:36:57 -0500

    WIP on IE11 upgrade with resizable state problem

--- test/test-hook.jsx
+++ test/test-hook.jsx
@@ -2,11 +2,10 @@
 var React = require('react');
 require('style!css!../css/styles.css');
 require('style!css!../examples/example-styles.css');
-require('style!css!../node_modules/react-resizable/css/styles.css');
 typeof window !== "undefined" && (window.React = React); // for devtools
 
 module.exports = function(Layout) {
-  document.addEventListener("DOMContentLoaded", function(event) { 
+  document.addEventListener("DOMContentLoaded", function(event) {
     var contentDiv = document.getElementById('content');
     var gridProps = window.gridProps || {};
     React.render(React.createElement(ExampleLayout, gridProps), contentDiv);
@@ -43,7 +42,7 @@ module.exports = function(Layout) {
         </div>
       );
     }
-    
+
   });
 };
 
