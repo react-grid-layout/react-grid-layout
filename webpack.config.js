0d366bcf16aa00643748fb970a5bb5db72dfba65
Samuel Reed <samuel.trace.reed@gmail.com>
2015-08-05 17:50:54 -0500

    Fix duplicated react in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,46 @@
-'use strict';
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 // Builds bundle usable <script>. Includes RGL and all deps, excluding React.
 module.exports = {
-    context: __dirname,
-    entry: {
-      'react-grid-layout': './index-dev.js'
-    },
-    output: {
-      path: __dirname + "/dist",
-      filename: "[name].min.js",
-      libraryTarget: "umd",
-      library: "ReactGridLayout"
-    },
-    devtool: 'source-map',
-    externals: {
-      // React dep should be available as window.React
-      "react": "React",
-      "react/addons": "React"
-    },
-    module: {
-      loaders: [
-        {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader?stage=0'}
-      ]
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        "process.env": {
-          NODE_ENV: JSON.stringify('production')
-        }
-      }),
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.optimize.DedupePlugin(),
-      // See #40 - duplicated React modules don't play nice
-      new webpack.NormalModuleReplacementPlugin(/\/react\/lib\/cloneWithProps/, '../../react-clonewithprops/index.js'),
-      // Compress, but don't print warnings to console
-      new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
-    ],
-    resolve: {
-      extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx"]
+  context: __dirname,
+  entry: {
+    "react-grid-layout": "./index-dev.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].min.js",
+    libraryTarget: "umd",
+    library: "ReactGridLayout"
+  },
+  devtool: "source-map",
+  externals: {
+    "react": {
+      "commonjs": "react",
+      "commonjs2": "react",
+      "amd": "react",
+      // React dep should be available as window.React, not window.react
+      "root": "React"
     }
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader?stage=0"}
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    // See #40 - duplicated React modules don't play nice
+    new webpack.NormalModuleReplacementPlugin(/\/react\/lib\/cloneWithProps/, "../../react-clonewithprops/index.js"),
+    // Compress, but don't print warnings to console
+    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+  ],
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx"]
+  }
 };
