ba16a4e8b48973bc9932a267531c97d53dc30ccb
Jason Henriksen <jhenriksen@hq.prv>
2016-06-20 16:47:03 -0700

    fixing some confused text

--- test/examples/5-static-elements.jsx
+++ test/examples/5-static-elements.jsx
@@ -26,7 +26,13 @@ var StaticElementsLayout = React.createClass({
         <div key="1" _grid={{x: 0, y: 0, w: 2, h: 3}}><span className="text">1</span></div>
         <div key="2" _grid={{x: 2, y: 0, w: 4, h: 3, static: true}}><span className="text">2 - Static</span></div>
         <div key="3" _grid={{x: 6, y: 0, w: 2, h: 3}}><span className="text">3</span></div>
-        <div key="4" _grid={{x: 2, y: 0, w: 4, h: 3, draggableHandle: '.react-grid-dragHandleExample'}}><span className="text">4 - Draggable with Handle<hr/><hr/><span className={}>[DRAG HERE]<hr/><hr></span></div>
+        <div key="4" _grid={{x: 2, y: 0, w: 4, h: 3, draggableHandle: '.react-grid-dragHandleExample'}}>
+          <span className="text">4 - Draggable with Handle
+            <hr/><hr/>
+            <span className='react-grid-dragHandleExample'>[DRAG HERE]</span>
+            <hr/><hr/>
+          </span>
+        </div>
       </ReactGridLayout>
     );
   }
