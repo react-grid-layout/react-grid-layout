85f8b187c9ba413ddbbe4fdb90d713cffa3729f7
jdkaplan <jdkaplan@users.noreply.github.com>
2016-08-03 19:50:56 -0400

    Change GridItem propTypes to avoid React warning (fix #303) (#306)

--- lib/GridItem.jsx
+++ lib/GridItem.jsx
@@ -35,25 +35,27 @@ export default class GridItem extends React.Component {
     h: PropTypes.number.isRequired,
 
     // All optional
-    minW: function (props, propName, componentName, location, propFullName) {
-      PropTypes.number(props, propName, componentName, location, propFullName);
+    minW: function (props, propName, componentName) {
       const value = props[propName];
+      if (typeof value !== 'number') return new Error('minWidth not Number');
       if (value > props.w || value > props.maxW) return new Error('minWidth larger than item width/maxWidth');
     },
-    maxW: function (props, propName, componentName, location, propFullName) {
-      PropTypes.number(props, propName, componentName, location, propFullName);
-      const value = props[propName];
-      if (value < props.w || value < props.minW) return new Error('maxWidth smaller than item width/minWidth');
 
-    },
-    minH: function (props, propName, componentName, location, propFullName) {
-      PropTypes.number(props, propName, componentName, location, propFullName);
+    maxW: function (props, propName, componentName) {
       const value = props[propName];
+      if (typeof value !== 'number') return new Error('maxWidth not Number');
+      if (value < props.w || value < props.minW) return new Error('maxWidth smaller than item width/minWidth');
+    },
+
+    minH: function (props, propName, componentName) {
+      const value = props[propName];
+      if (typeof value !== 'number') return new Error('minHeight not Number');
       if (value > props.h || value > props.maxH) return new Error('minHeight larger than item height/maxHeight');
     },
-    maxH: function (props, propName, componentName, location, propFullName) {
-      PropTypes.number(props, propName, componentName, location, propFullName);
+
+    maxH: function (props, propName, componentName) {
       const value = props[propName];
+      if (typeof value !== 'number') return new Error('maxHeight not Number');
       if (value < props.h || value < props.minH) return new Error('maxHeight smaller than item height/minHeight');
     },
 
