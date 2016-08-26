3b75204591840665adefeb4cd8c6080aa94b8393
Wes Hildebrandt <wes.hildebrandt@digitalglobe.com>
2016-08-26 08:22:11 -0500

    Add WidthProvider option to await nonzero width

--- lib/components/WidthProvider.jsx
+++ lib/components/WidthProvider.jsx
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom';
 
 type State = {
   mounted: boolean,
-  width: number
+  width: number,
+  widthFound: boolean,
 };
 
 /*
@@ -14,18 +15,29 @@ type State = {
 export default (ComposedComponent: ReactClass): ReactClass => class extends React.Component {
 
   static defaultProps = {
-    measureBeforeMount: false
+    measureBeforeMount: false,
+    awaitNonzeroWidth: false,
+    nonzeroWidthRetryDelay: 50,
+    nonzeroWidthMaxWait: 2000,
   };
 
   static propTypes = {
     // If true, will not render children until mounted. Useful for getting the exact width before
     // rendering, to prevent any unsightly resizing.
-    measureBeforeMount: React.PropTypes.bool
+    measureBeforeMount: React.PropTypes.bool,
+
+    // If true, will not render children until a nonzero width is detected (or maxDelay has been exceeded)
+    awaitNonzeroWidth: React.PropTypes.bool,
+    // Millisecond delay between checks for nonzero width
+    nonzeroWidthRetryDelay: React.PropTypes.number,
+    // Max time in milliseconds to check for nonzero width
+    nonzeroWidthMaxWait: React.PropTypes.number,
   };
 
   state: State = {
     mounted: false,
-    width: 1280
+    width: 1280,
+    widthFound: false,
   };
 
   componentDidMount() {
@@ -44,11 +56,32 @@ export default (ComposedComponent: ReactClass): ReactClass => class extends Reac
 
   onWindowResize = (_event: Event, cb: ?Function) => {
     const node = ReactDOM.findDOMNode(this);
-    this.setState({width: node.offsetWidth}, cb);
+    const width = node.offsetWidth;
+    if (!this.props.awaitNonzeroWidth || this.state.widthFound || width != 0) {
+      this.setState({width, widthFound: true}, cb);
+    }
+    else {
+      let loopCounter = 0;
+      const that = this;
+      const waitForNonZeroWidth = function() {
+        const node = ReactDOM.findDOMNode(that);
+        const width = node.offsetWidth;
+        if (width == 0) {
+          loopCounter++;
+          const wait = loopCounter++ * that.props.nonzeroWidthRetryDelay;
+          if (wait >= that.props.nonzeroWidthMaxWait) that.setState({widthFound: true}, cb);
+          else setTimeout(waitForNonZeroWidth, that.props.nonzeroWidthRetryDelay);
+        }
+        else that.setState({width, widthFound: true}, cb);
+      };
+      waitForNonZeroWidth();
+    }
   }
 
   render() {
-    if (this.props.measureBeforeMount && !this.state.mounted) return <div {...this.props} {...this.state} />;
+    // the internal <div /> prevents child components from being rendered too early
+    if (this.props.awaitNonzeroWidth && !this.state.widthFound) return <div {...this.props} {...this.state} ><div /></div>;
+    else if (this.props.measureBeforeMount && !this.state.mounted) return <div {...this.props} {...this.state} />;
     return <ComposedComponent {...this.props} {...this.state} />;
   }
 };
