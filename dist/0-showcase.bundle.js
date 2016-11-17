fa5c2030456b6bc2d5c137bce8868ea9f6a8be2b
Samuel Reed <samuel.trace.reed@gmail.com>
2016-11-17 11:39:02 -0600

    Rebuild with React 15.4

--- dist/0-showcase.bundle.js
+++ dist/0-showcase.bundle.js
@@ -8,15 +8,15 @@ webpackJsonp([13],[
 
 	var _jsx = function () { var REACT_ELEMENT_TYPE = typeof Symbol === "function" && Symbol.for && Symbol.for("react.element") || 0xeac7; return function createRawReactElement(type, props, key, children) { var defaultProps = type && type.defaultProps; var childrenLength = arguments.length - 3; if (!props && childrenLength !== 0) { props = {}; } if (props && defaultProps) { for (var propName in defaultProps) { if (props[propName] === void 0) { props[propName] = defaultProps[propName]; } } } else if (!props) { props = defaultProps || {}; } if (childrenLength === 1) { props.children = children; } else if (childrenLength > 1) { var childArray = Array(childrenLength); for (var i = 0; i < childrenLength; i++) { childArray[i] = arguments[i + 3]; } props.children = childArray; } return { $$typeof: REACT_ELEMENT_TYPE, type: type, key: key === undefined ? null : '' + key, ref: null, props: props, _owner: null }; }; }();
 
-	var _react = __webpack_require__(7);
+	var _react = __webpack_require__(8);
 
 	var _react2 = _interopRequireDefault(_react);
 
-	var _lodash = __webpack_require__(15);
+	var _lodash = __webpack_require__(16);
 
 	var _lodash2 = _interopRequireDefault(_lodash);
 
-	var _reactGridLayout = __webpack_require__(3);
+	var _reactGridLayout = __webpack_require__(4);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -123,9 +123,9 @@ webpackJsonp([13],[
 	}
 
 	if (__webpack_require__.c[0] === module) {
-	  __webpack_require__(12)(module.exports);
+	  __webpack_require__(13)(module.exports);
 	}
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(9)(module)))
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(11)(module)))
 
 /***/ }
 ]);
--- dist/4-grid-property.bundle.js
+++ dist/4-grid-property.bundle.js
@@ -8,11 +8,11 @@ webpackJsonp([6],[
 
 	var _jsx = function () { var REACT_ELEMENT_TYPE = typeof Symbol === "function" && Symbol.for && Symbol.for("react.element") || 0xeac7; return function createRawReactElement(type, props, key, children) { var defaultProps = type && type.defaultProps; var childrenLength = arguments.length - 3; if (!props && childrenLength !== 0) { props = {}; } if (props && defaultProps) { for (var propName in defaultProps) { if (props[propName] === void 0) { props[propName] = defaultProps[propName]; } } } else if (!props) { props = defaultProps || {}; } if (childrenLength === 1) { props.children = children; } else if (childrenLength > 1) { var childArray = Array(childrenLength); for (var i = 0; i < childrenLength; i++) { childArray[i] = arguments[i + 3]; } props.children = childArray; } return { $$typeof: REACT_ELEMENT_TYPE, type: type, key: key === undefined ? null : '' + key, ref: null, props: props, _owner: null }; }; }();
 
-	var React = __webpack_require__(7);
-	var PureRenderMixin = __webpack_require__(4);
-	var _ = __webpack_require__(15);
-	var WidthProvider = __webpack_require__(3).WidthProvider;
-	var ReactGridLayout = __webpack_require__(3);
+	var React = __webpack_require__(8);
+	var PureRenderMixin = __webpack_require__(6);
+	var _ = __webpack_require__(16);
+	var WidthProvider = __webpack_require__(4).WidthProvider;
+	var ReactGridLayout = __webpack_require__(4);
 	ReactGridLayout = WidthProvider(ReactGridLayout);
 
 	var GridPropertyLayout = React.createClass({
@@ -70,15 +70,17 @@ webpackJsonp([6],[
 	module.exports = GridPropertyLayout;
 
 	if (__webpack_require__.c[0] === module) {
-	  __webpack_require__(12)(module.exports);
+	  __webpack_require__(13)(module.exports);
 	}
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(9)(module)))
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(11)(module)))
 
 /***/ },
 /* 1 */,
 /* 2 */,
 /* 3 */,
-/* 4 */
+/* 4 */,
+/* 5 */,
+/* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/**
@@ -89,12 +91,11 @@ webpackJsonp([6],[
 	 * LICENSE file in the root directory of this source tree. An additional grant
 	 * of patent rights can be found in the PATENTS file in the same directory.
 	 *
-	 * @providesModule ReactComponentWithPureRenderMixin
 	 */
 
 	'use strict';
 
-	var shallowCompare = __webpack_require__(5);
+	var shallowCompare = __webpack_require__(7);
 
 	/**
 	 * If your React component's render function is "pure", e.g. it will render the
@@ -119,6 +120,8 @@ webpackJsonp([6],[
 	 * complex data structures this mixin may have false-negatives for deeper
 	 * differences. Only mixin to components which have simple props and state, or
 	 * use `forceUpdate()` when you know deep data structures have changed.
+	 *
+	 * See https://facebook.github.io/react/docs/pure-render-mixin.html
 	 */
 	var ReactComponentWithPureRenderMixin = {
 	  shouldComponentUpdate: function (nextProps, nextState) {
@@ -129,7 +132,7 @@ webpackJsonp([6],[
 	module.exports = ReactComponentWithPureRenderMixin;
 
 /***/ },
-/* 5 */
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/**
@@ -140,16 +143,16 @@ webpackJsonp([6],[
 	 * LICENSE file in the root directory of this source tree. An additional grant
 	 * of patent rights can be found in the PATENTS file in the same directory.
 	 *
-	* @providesModule shallowCompare
-	*/
+	 */
 
 	'use strict';
 
-	var shallowEqual = __webpack_require__(11);
+	var shallowEqual = __webpack_require__(10);
 
 	/**
 	 * Does a shallow comparison for props and state.
 	 * See ReactComponentWithPureRenderMixin
+	 * See also https://facebook.github.io/react/docs/shallow-compare.html
 	 */
 	function shallowCompare(instance, nextProps, nextState) {
 	  return !shallowEqual(instance.props, nextProps) || !shallowEqual(instance.state, nextState);
