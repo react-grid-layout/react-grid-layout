082a080d0328467459aaa84de1df0fde83b12199
Bogdan Chadkin <trysound@yandex.ru>
2017-12-12 12:35:35 +0300

    Add external drag api

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-module.exports = require('./build/ReactGridLayout').default;
-module.exports.utils = require('./build/utils');
-module.exports.Responsive = require('./build/ResponsiveReactGridLayout').default;
-module.exports.Responsive.utils = require('./build/responsiveUtils');
-module.exports.WidthProvider = require('./build/components/WidthProvider').default;
+const utils = require("./build/utils");
+
+module.exports = require("./build/ReactGridLayout").default;
+module.exports.createDragApiRef = utils.createDragApiRef;
+module.exports.utils = utils;
+module.exports.Responsive = require("./build/ResponsiveReactGridLayout").default;
+module.exports.Responsive.utils = require("./build/responsiveUtils");
+module.exports.WidthProvider = require("./build/components/WidthProvider").default;
