2bcdf7d606f792214c1ae0d0225161e44ffe956a
Rolando Arteaga <Rolando.Arteaga@jalasoft.com>
2018-05-02 17:39:40 -0400

    remove react PropTypes from GridItem, WidthProvider and ReactGridLayout

--- lib/GridItem.jsx
+++ lib/GridItem.jsx
@@ -1,5 +1,6 @@
 // @flow
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {DraggableCore} from 'react-draggable';
 import {Resizable} from 'react-resizable';
 import {perc, setTopLeft, setTransform} from './utils';
