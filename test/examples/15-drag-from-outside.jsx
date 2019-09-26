fbbcbc61995a1b6bb7228c4da0009d28f7a747ba
Sergey Golovin <golovim@gmail.com>
2019-09-26 11:12:27 +0300

    fix isDroppable in firefox

--- test/examples/15-drag-from-outside.jsx
+++ test/examples/15-drag-from-outside.jsx
@@ -88,7 +88,16 @@ class ShowcaseLayout extends React.Component {
         <button onClick={this.onCompactTypeChange}>
           Change Compaction Type
         </button>
-        <div className="droppable-element" draggable={true} unselectable="on">
+        <div
+          className="droppable-element"
+          draggable={true}
+          unselectable="on"
+          // this is a hack for firefox
+          // Firefox requires some kind of initialization
+          // which we can do by adding this attribute
+          // @see https://bugzilla.mozilla.org/show_bug.cgi?id=568313
+          onDragStart={e => e.dataTransfer.setData("text/plain", "")}
+        >
           Droppable Element
         </div>
         <ResponsiveReactGridLayout
