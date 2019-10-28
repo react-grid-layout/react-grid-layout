53425b8c47da56c2fe6996bbaebc35554df5a755
Thomas Gorgolione <thomas.gorgolione@olympus.com>
2019-10-28 12:16:17 -0400

    Added new build command to create unminified-nonprod js

---
+++ webpack-nomin.config.js
@@ -0,0 +1,58 @@
+var webpack = require("webpack");
+
+// Builds bundle usable <script>. Includes RGL and all deps, excluding React.
+module.exports = {
+  mode: "development",
+  optimization: {
+    minimize: false
+  },
+  context: __dirname,
+  entry: {
+    "react-grid-layout": "./index-dev.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "ReactGridLayout"
+  },
+  devtool: "source-map",
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      // React dep should be available as window.React, not window.react
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          cacheDirectory: true
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
