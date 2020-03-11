9375a43badffac550226bcef510e5504784e51b3
Samuel Reed <sam@bitmex.com>
2020-03-11 16:56:59 -0400

    fix(showcase): make random placement more appealing

--- test/examples/0-showcase.jsx
+++ test/examples/0-showcase.jsx
@@ -129,7 +129,7 @@ function generateLayout() {
   return _.map(_.range(0, 25), function(item, i) {
     var y = Math.ceil(Math.random() * 4) + 1;
     return {
-      x: Math.ceil(Math.random() * 10),
+      x: Math.round(Math.random() * 5) * 2,
       y: Math.floor(i / 6) * y,
       w: 2,
       h: y,
