d146b95462ce69a6dd0270a2153cc9cc564ee073
Samuel Reed <sam@bitmex.com>
2020-03-11 16:43:12 -0400

    test(travis): ensure node 12 sort algo doesn't blow up tests

--- test/util/setupTests.js
+++ test/util/setupTests.js
@@ -1,6 +1,16 @@
 // @flow
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// We rely on sort() being deterministic for tests, but it changed from QuickSort to TimSort
+// in Node 12. This breaks tests, so we monkey-patch it.
+import { sort } from "timsort";
+
+// $FlowIgnore dirty hack
+Array.prototype.sort = function(comparator) {
+  sort(this, comparator);
+  return this;
+};
