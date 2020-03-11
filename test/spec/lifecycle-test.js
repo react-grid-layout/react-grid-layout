ca7957b70fc8147c2694c458c996ab699fd2a2c5
Samuel Reed <sam@bitmex.com>
2020-03-11 13:48:45 -0400

    test(RRGL): test that layouts is not modified

--- test/spec/lifecycle-test.js
+++ test/spec/lifecycle-test.js
@@ -2,8 +2,12 @@
 /* eslint-env jest */
 
 import React from 'react';
+import _ from 'lodash';
+import ResponsiveReactGridLayout from '../../lib/ResponsiveReactGridLayout';
 import ReactGridLayout from '../../lib/ReactGridLayout';
 import BasicLayout from '../examples/1-basic';
+import ShowcaseLayout from '../examples/0-showcase';
+import deepFreeze from '../util/deepFreeze';
 import Chance from 'chance';
 import {shallow, mount} from 'enzyme';
 
@@ -24,6 +28,34 @@ describe('Lifecycle tests', function() {
       const wrapper = shallow(<BasicLayout />);
 
       expect(wrapper).toMatchSnapshot();
+    });
+
+    it('Does not modify layout on movement', async function() {
+      const layouts = {
+        lg: [
+          ..._.times(3, (i) => ({
+            i: String(i),
+            x: i,
+            y: 0,
+            w: 1,
+            h: 1,
+          }))
+        ]
+      };
+      const frozenLayouts = deepFreeze(layouts, {set: true, get: false /* don't crash on unknown gets */});
+      // Render the basic Responsive layout.
+      const wrapper = mount(
+        <ResponsiveReactGridLayout layouts={frozenLayouts} width={1280} breakpoint="lg">
+          {_.times(3, (i) => <div key={i} />)}
+        </ResponsiveReactGridLayout>
+      );
+
+      // Set that layout as state and ensure it doesn't change.
+      wrapper.setState({layouts: frozenLayouts});
+      wrapper.setProps({width: 800, breakpoint: 'md'}); // will generate new layout
+      wrapper.render();
+
+      expect(frozenLayouts).not.toContain('md');
     });
   });
 });
