e6879dda471fe0a2cfdf623459dd2758e3a6955d
Samuel Reed <samuel.trace.reed@gmail.com>
2022-01-04 12:32:01 -0500

    chore(dev): rework lint action and proper pre-commit linting

--- index-dev.js
+++ index-dev.js
@@ -2,4 +2,5 @@ module.exports = require("./lib/ReactGridLayout").default;
 module.exports.utils = require("./lib/utils");
 module.exports.Responsive = require("./lib/ResponsiveReactGridLayout").default;
 module.exports.Responsive.utils = require("./lib/responsiveUtils");
-module.exports.WidthProvider = require("./lib/components/WidthProvider").default;
+module.exports.WidthProvider =
+  require("./lib/components/WidthProvider").default;
