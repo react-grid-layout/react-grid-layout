388b79fb52fabfe107f946fbad9ffacd63324d09
Eric <ericjlaursen@gmail.com>
2022-01-06 12:33:11 -0500

    test(utils): additional tests for calcWH and calcXY (#1637)
    * tests calcWH and calcXY
    
    * tests compactType and extra Error throwing
    
    * ajust "it" string on throw test
    
    * err on layout item i not as a string
    
    * smoke test on deepFreeze
    
    * formatting
    
    * chore(dev): rework lint action and proper pre-commit linting
    
    * remove in-favor of trusting type-checking
    
    * remove type-checked tests && include set in smoke test for "deepFreeze"
    
    * enforce coverageThreshold during Makefile test command
    
    Co-authored-by: Samuel Reed <samuel.trace.reed@gmail.com>

--- test/spec/utils-test.js
+++ test/spec/utils-test.js
@@ -8,13 +8,17 @@ import {
   fastRGLPropsEqual,
   moveElement,
   sortLayoutItemsByRowCol,
-  validateLayout
+  validateLayout,
+  compactType
 } from "../../lib/utils";
 import {
   calcGridColWidth,
-  calcGridItemPosition
+  calcGridItemPosition,
+  calcWH,
+  calcXY
 } from "../../lib/calculateUtils";
 import isEqual from "lodash.isequal";
+import deepFreeze from "./../util/deepFreeze";
 
 describe("bottom", () => {
   it("Handles an empty layout as input", () => {
@@ -73,7 +77,7 @@ describe("validateLayout", () => {
       { i: "2", x: 1, y: 2, w: 1, h: 1 }
     ]);
   });
-  it("Throws errors on invalid input", () => {
+  it("Throws errors on h not as a number", () => {
     expect(() => {
       validateLayout([
         { i: "1", x: 0, y: 1, w: 1, h: 1 },
@@ -606,5 +610,89 @@ describe("fastRGLPropsEqual", () => {
       somethingElse: { w: 1, h: 2, i: 3 }
     };
     expect(fastRGLPropsEqual(props1, props2, isEqual)).toEqual(true);
+  });
+});
+
+describe("calcWH", () => {
+  const mockPositionParams = {
+    margin: [0, 0],
+    containerPadding: [0, 0],
+    containerWidth: 400,
+    cols: 4,
+    rowHeight: 200,
+    maxRows: 3
+  };
+  it("return { w: 1, h: 1 }", () => {
+    const res = calcWH(mockPositionParams, 100, 200, 1, 1);
+    expect(JSON.stringify(res)).toBe(JSON.stringify({ w: 1, h: 1 }));
+  });
+  it("return { w: 2, h: 1 }", () => {
+    const res = calcWH(mockPositionParams, 200, 200, 1, 1);
+    expect(JSON.stringify(res)).toBe(JSON.stringify({ w: 2, h: 1 }));
+  });
+  it("return { w: 1, h: 2 }", () => {
+    const res = calcWH(mockPositionParams, 100, 400, 1, 1);
+    expect(JSON.stringify(res)).toBe(JSON.stringify({ w: 1, h: 2 }));
+  });
+});
+
+describe("calcXY", () => {
+  const mockPositionParams = {
+    margin: [0, 0],
+    containerPadding: [0, 0],
+    containerWidth: 500,
+    cols: 4,
+    rowHeight: 100,
+    maxRows: 3
+  };
+
+  it("return {x:0, y:0}", () => {
+    const TOP = 10;
+    const LEFT = 10;
+    const W = 300;
+    const H = 100;
+    const res = calcXY(mockPositionParams, TOP, LEFT, W, H);
+    expect(JSON.stringify(res)).toBe(JSON.stringify({ x: 0, y: 0 }));
+  });
+  it("return {x:1, y:0}", () => {
+    const TOP = 0;
+    const LEFT = 100;
+    const W = 0;
+    const H = 0;
+    const res = calcXY(mockPositionParams, TOP, LEFT, W, H);
+    expect(JSON.stringify(res)).toBe(JSON.stringify({ x: 1, y: 0 }));
+  });
+  it("return {x:0, y:1}", () => {
+    const TOP = 110;
+    const LEFT = 0;
+    const W = 0;
+    const H = 0;
+    const res = calcXY(mockPositionParams, TOP, LEFT, W, H);
+    expect(JSON.stringify(res)).toBe(JSON.stringify({ x: 0, y: 1 }));
+  });
+});
+
+describe("compactType", () => {
+  const mockProps = {
+    verticalCompact: false,
+    compactType: "horizontal"
+  };
+  it("returns null when verticalCompact is false", () => {
+    expect(compactType(mockProps)).toBe(null);
+  });
+  it("returns compactType value when verticalCompact is true", () => {
+    expect(compactType({ ...mockProps, verticalCompact: true })).toBe(
+      "horizontal"
+    );
+  });
+});
+
+describe("deepFreeze", () => {
+  it("smoke test", () => {
+    const deepFreezeResult = deepFreeze(
+      { a: "a", b: { b: "c" } },
+      { get: true, set: true }
+    );
+    expect(JSON.stringify(deepFreezeResult)).toBe('{"a":"a","b":{"b":"c"}}');
   });
 });
