229f02cf301b80ae0a46d679b50e802f8d8d89dc
STRML <samuel.trace.reed@gmail.com>
2022-02-21 20:15:26 +0000

    Deploying to gh-pages from @ react-grid-layout/react-grid-layout@ee90826a551fe5c3d58a56a4237a423373ac48f1 🚀

--- examples/7-localstorage.js
+++ examples/7-localstorage.js
@@ -189,7 +189,7 @@
 /******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
 /******/ 					installedChunks[chunkId][0]();
 /******/ 				}
-/******/ 				installedChunks[chunkIds[i]] = 0;
+/******/ 				installedChunks[chunkId] = 0;
 /******/ 			}
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
