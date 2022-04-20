d113baa3977f1aae6555f6f8031685e0c576028b
Sarah Mihuc <mihuc@whitewhaleanalytics.com>
2022-04-20 13:35:20 -0400

    made gap filling example simpler

--- test/examples/20-fill-gaps.jsx
+++ test/examples/20-fill-gaps.jsx
@@ -1,134 +1,77 @@
 import React from "react";
 import _ from "lodash";
-import { Responsive, WidthProvider } from "react-grid-layout";
+import RGL, { WidthProvider } from "react-grid-layout";
 
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
+const ReactGridLayout = WidthProvider(RGL);
 
 export default class FillGapsLayout extends React.Component {
   static defaultProps = {
     className: "layout",
     rowHeight: 200,
     onLayoutChange: function() {},
-    cols: { lg: 4, md: 3, sm:2, xs: 1, xxs: 1 },
-    initialLayout: generateLayout()
+    cols: 8,
   };
 
-  state = {
-    currentBreakpoint: "lg",
-    compactType: "vertical",
-    mounted: false,
-    layouts: { lg: this.props.initialLayout }
-  };
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-    this.setState({ mounted: true });
+    const layout = this.generateLayout();
+    this.state = { layout };
   }
 
   generateDOM() {
-    return _.map(this.state.layouts.lg, function(l, i) {
+    return _.map(this.state.layout, function(item) {
       return (
-        <div key={i} className={l.hideOnDrag ? "static" : ""}>
-          {l.hideOnDrag ? (
-            <span
-              className="text"
-              title="This item is static and cannot be removed or resized."
-            >
-              hideOnDrag - {i}
-            </span>
-          ) : (
-            <span className="text">{l.hideOnDrag ? `hideOnDrag-${i}` : i}</span>
-          )}
+        <div key={item.i}>
+          <span className="text">{item.i}</span>
         </div>
       );
     });
   }
 
-  onBreakpointChange = breakpoint => {
-    this.setState({
-      currentBreakpoint: breakpoint
+  generateLayout() {
+    return _.map(_.range(0, 20), function(item, i) {
+      var y = Math.ceil(Math.random() * 4) + 1;
+      const minH = Math.ceil(1);
+      const maxH = Math.floor(3);
+      const maxW = Math.floor(4);
+      return {
+        x: (_.random(0, 4)) % 6,
+        y: Math.floor(i / 6) * y,
+        w: Math.floor(Math.random() * (maxW - minH)) + minH,
+        h: Math.floor(Math.random() * (maxH - minH)) + minH,
+        i: i.toString(),
+      };
     });
-  };
+  }
 
-  onCompactTypeChange = () => {
-    const { compactType: oldCompactType } = this.state;
-    const compactType =
-      oldCompactType === "horizontal"
-        ? "vertical"
-        : oldCompactType === "vertical" ? null : "horizontal";
-    this.setState({ compactType });
-  };
-
-  onLayoutChange = (layout, layouts) => {
-    this.props.onLayoutChange(layout, layouts);
-  };
-
-  onNewLayout = () => {
-    this.setState({
-      layouts: { lg: generateLayout() }
-    });
-  };
+  addCard(item) {
+    this.setState({ layout: [...this.state.layout, { ...item, i: this.state.layout.length.toString(), gap: false }] })
+  }
 
   render() {
     return (
       <div>
-        <div>
-          Current Breakpoint: {this.state.currentBreakpoint} ({
-            this.props.cols[this.state.currentBreakpoint]
-          }{" "}
-          columns)
-        </div>
-        <div>
-          Compaction type:{" "}
-          {_.capitalize(this.state.compactType) || "No Compaction"}
-        </div>
-        <button onClick={this.onNewLayout}>Generate New Layout</button>
-        <button onClick={this.onCompactTypeChange}>
-          Change Compaction Type
-        </button>
-        <ResponsiveReactGridLayout
+        <ReactGridLayout
           {...this.props}
-          layouts={this.state.layouts}
-          onBreakpointChange={this.onBreakpointChange}
-          onLayoutChange={this.onLayoutChange}
+          layout={this.state.layout}
+          onLayoutChange={this.props.onLayoutChange}
+          useCSSTransforms={true}
           heightUnits={3}
           // Gap props
           fillGaps={true}
           lastRowGap={true}
           gapRenderFunction={(gap) => {
               return (
-                  <div style={{ height: 'inherit', backgroundColor: 'green' }}>{gap.i}</div>
+                  <div style={{ height: 'inherit', backgroundColor: 'gray' }} onClick={() => this.addCard(gap)}><span className="text">Gap filler: click to add</span></div>
               );
           }}
-          // WidthProvider option
-          measureBeforeMount={false}
-          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
-          // and set `measureBeforeMount={true}`.
-          useCSSTransforms={this.state.mounted}
-          compactType={this.state.compactType}
-          preventCollision={!this.state.compactType}
         >
           {this.generateDOM()}
-        </ResponsiveReactGridLayout>
+        </ReactGridLayout>
       </div>
     );
   }
-}
-
-function generateLayout() {
-  return _.map(_.range(0, 3), function(item, i) {
-    var y = Math.ceil(Math.random() * 4) + 1;
-    const minH = Math.ceil(1);
-    const maxH = Math.floor(3);
-    const maxW = Math.floor(4);
-    return {
-      x: (_.random(0, 4)) % 6,
-      y: Math.floor(i / 6) * y,
-      w: Math.floor(Math.random() * (maxW - minH)) + minH,
-      h: Math.floor(Math.random() * (maxH - minH)) + minH,
-      i: i.toString(),
-    };
-
-  });
 }
 
 if (process.env.STATIC_EXAMPLES === true) {
