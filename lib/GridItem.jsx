7880c2917831a11b70c076e16e51836dda190e88
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-03-21 11:06:52 +0900

    FIX: props containerPadding destructuring

--- lib/GridItem.jsx
+++ lib/GridItem.jsx
@@ -486,7 +486,7 @@ export default class GridItem extends React.Component<Props, State> {
       const { offsetParent } = node;
 
       if (offsetParent) {
-        const { margin, rowHeight } = this.props;
+        const { margin, rowHeight, containerPadding } = this.props;
         const bottomBoundary =
           offsetParent.clientHeight - calcGridItemWHPx(h, rowHeight, margin[1]);
         top = clamp(top - containerPadding[1], 0, bottomBoundary);
@@ -502,6 +502,7 @@ export default class GridItem extends React.Component<Props, State> {
     this.setState({ dragging: newPosition });
 
     // Call callback with this data
+    const { containerPadding } = this.props;
     const { x, y } = calcXY(positionParams, top - containerPadding[1], left - containerPadding[0], w, h);
     return onDrag.call(this, i, x, y, {
       e,
@@ -522,7 +523,7 @@ export default class GridItem extends React.Component<Props, State> {
     if (!this.state.dragging) {
       throw new Error("onDragEnd called before onDragStart.");
     }
-    const { w, h, i } = this.props;
+    const { w, h, i, containerPadding } = this.props;
     const { left, top } = this.state.dragging;
     const newPosition: PartialPosition = { top, left };
     this.setState({ dragging: null });
