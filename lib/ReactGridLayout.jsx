bcb59f426312cb03516127e5330f5ac117c6e5ad
Antonio Santos <antoniosantos@pop-os.localdomain>
2023-08-04 16:43:19 -0400

    fix position logic when item is dragged in from the outside

--- lib/ReactGridLayout.jsx
+++ lib/ReactGridLayout.jsx
@@ -659,8 +659,12 @@ export default class ReactGridLayout extends React.Component<Props, State> {
     const finalDroppingItem = { ...droppingItem, ...onDragOverResult };
 
     const { layout } = this.state;
-    // This is relative to the DOM element that this event fired for.
-    const { layerX, layerY } = e.nativeEvent;
+
+    const gridRect = e.currentTarget.getBoundingClientRect() // The grid's position in the viewport
+
+    // Calculate the mouse position relative to the grid
+    const layerX = e.clientX - gridRect.left / transformScale
+    const layerY = e.clientY - gridRect.top / transformScale
     const droppingPosition = {
       left: layerX / transformScale,
       top: layerY / transformScale,
