8bb97b56be4cc221869ea84e69b937574b656390
chengshuyan123 <chengshuyan@winktech.sg>
2025-04-22 10:32:40 +0800

    feat: 去掉util导出

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 module.exports = require("./build/ReactGridLayout").default;
-module.exports.utils = require("./build/utils");
-module.exports.calculateUtils = require("./build/calculateUtils");
-module.exports.Responsive =
-  require("./build/ResponsiveReactGridLayout").default;
-module.exports.Responsive.utils = require("./build/responsiveUtils");
-module.exports.WidthProvider =
-  require("./build/components/WidthProvider").default;
+// module.exports.utils = require("./build/utils");
+// module.exports.calculateUtils = require("./build/calculateUtils");
+
+// module.exports.Responsive =require("./build/ResponsiveReactGridLayout").default;
+// module.exports.Responsive.utils = require("./build/responsiveUtils");
+// module.exports.WidthProvider =require("./build/components/WidthProvider").default;
